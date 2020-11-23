80086dcc22148042188965485690c1e23a639f3b
Victor Westerlund <victor.vesterlund@gmail.com>
2020-11-23 10:48:42 +0100

    Added PyPi project packaging
    Initiall PyPi build

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md","r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+    name="pysheeter-VicW",
+    version="1.0.0",
+    author="VicW",
+    author_email="victor.vesterlund@gmail.com",
+    description="Lightweight Python-script to create sprite sheets from transparent PNGs with Pillow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VictorWesterlund/pysheeter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	install_requires=[
+        "Pillow",
+    ],
+    python_requires='>=3.6',
+)
