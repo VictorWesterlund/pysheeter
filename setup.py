4d381746cc5081106058b7f5e7a187b49be3a256
Victor Westerlund <victor.vesterlund@gmail.com>
2020-11-23 11:03:57 +0100

    Added "Pillow" as package dependancy
    Pillow will now be cloned along with PySheeter when installed with "pip3"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="pysheeter-VicW",
-    version="1.0.0",
+    version="1.0.1",
     author="VicW",
     author_email="victor.vesterlund@gmail.com",
     description="Lightweight Python-script to create sprite sheets from transparent PNGs with Pillow",
