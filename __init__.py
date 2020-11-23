d174fe948ad529957e53f0cbed6377a12066716f
Victor Westerlund <victor.vesterlund@gmail.com>
2020-11-23 17:51:29 +0100

    1.0.5

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from pysheeter import PySheeter
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="pysheeter",
-    version="1.0.4",
+    version="1.0.5",
     author="VicW",
     author_email="victor.vesterlund@gmail.com",
     description="Lightweight Python-script to create sprite sheets from transparent PNGs with Pillow",
