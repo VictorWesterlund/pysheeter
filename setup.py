4819eced5eefcde71dd551b680ae1240fb195c79
Victor Westerlund <victor.vesterlund@gmail.com>
2023-06-17 14:04:38 +0200

    fix(versioning): version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pysheeter",
-    version = "2.0.0",
+    version = "1.1.0",
     description = "Simple python library for creating sprite sheets from images",
     author = "Victor Westerlund",
     author_email = "victor.vesterlund@gmail.com",
